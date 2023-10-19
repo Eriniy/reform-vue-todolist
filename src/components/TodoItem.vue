5606d0075861dd85232d788eb9c33d7d1068aaa8
Krivy <yul.krivaya@bk.ru>
2023-10-19 23:11:37 +0300

    feat: added function for changing task

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,7 +4,17 @@
       :todo="todo"
       @click="toggleCompleted(index)"
     />
-    <p>{{ todo.text }}</p>
+    <div class="todo__list-item-text">
+      <div v-if="!this.todoEditing" class="todo__list-item-label"
+        @dblclick="editTodo(todo)"
+      >{{ todo.text }}</div>
+      <input v-else ref="myInput" class="todo__list-item-edit" type="text"
+        v-model="localTodo.text"
+        @blur="doneEdit(localTodo)"
+        @keyup.enter="doneEdit(localTodo)"
+        v-focus
+      >
+    </div>
     <remove-button
       @click="removeTodo(index)"
     />
@@ -25,12 +35,37 @@ export default {
       require: true,
     },
   },
+  data() {
+    return {
+      localTodo: this.todo,
+      todoEditing: false,
+      beforeEditCache: ''
+    }
+  },
+  directives: {
+    focus: {
+      mounted: function (el) {
+        console.log(el)
+        el.focus()
+      }
+    }
+  },  
   methods: {
     removeTodo(index) {
       this.$emit('onRemove', index)
     },
     toggleCompleted(index) {
       this.$emit('onToggleCompleted', index) 
+    },
+    editTodo(todo) {
+      this.beforeEditCache = todo.text
+      this.todoEditing = true
+    },
+    doneEdit(todo) {
+      if (todo.text.trim() == '') {
+        todo.text = this.beforeEditCache
+      }
+      this.todoEditing = false
     }
   }
 }
